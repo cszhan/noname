3a33a05d315ec254ca8a94e3af629462f031c4bc
cszhan <mac@Macintosh.local>
2012-08-03 00:31:37 +0800

    city data add

---
+++ DressMemo/GDataMgr/UICityPickerViewDataSourceBase.h
@@ -0,0 +1,13 @@
+//
+//  UICityPickerViewDataSourceBase.h
+//  DressMemo
+//
+//  Created by cszhan on 12-8-2.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICityPickerViewDataSourceBase : UIPickerViewDataSourceBase
+
+@end
---
+++ DressMemo/GDataMgr/UICityPickerViewDataSourceBase.m
@@ -0,0 +1,13 @@
+//
+//  UICityPickerViewDataSourceBase.m
+//  DressMemo
+//
+//  Created by cszhan on 12-8-2.
+//
+//
+
+#import "UICityPickerViewDataSourceBase.h"
+
+@implementation UICityPickerViewDataSourceBase
+
+@end
